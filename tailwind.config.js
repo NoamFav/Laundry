c8b1d45fc9e8c5ff474400934239ec5bd191ebd8
NoamFav <95364866+NoamFav@users.noreply.github.com>
2026-01-20 19:10:27 +0100

    fix(HEAD): update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx,vue}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+    plugins: [],
+};
